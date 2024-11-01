ad7cb4ff9f3f1f48880ed721388955126127430c
Abdelnour <ab.fellah@esi-sba.dz>
2024-11-01 11:30:03 +0100

    about

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -11,14 +11,15 @@ export function About(props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLEl
     return (
         <Section title="About" description="A little bit about my self" {...props} id='about' >
             <div className="flex flex-col sm:flex-row sm:gap-20 gap-8 w-full" ref={ref} >
-                <div className="sm:w-5/12 flex items-center" >
+                {/*<div className="sm:w-5/12 flex items-center" >
                     <img src={content?.images.about} alt="" className="sm:w-full w-8/12 mx-auto rounded-xl" />
-                </div>
-                <div className="sm:w-7/12" >
-                    <p className="text-gray-500 max-w-[35ch] mb-4" >
+                </div>*/}
+                <div className="mx-auto" >
+                    <p className="text-gray-500 sm:max-w-[50ch] mb-4" >
+                        My name is Abdelnour and I am last year AI &amp; Data Science Student and the higher school of computer science in Sidi Bel Abess.
                         I am a dedicated Machine Learning Engineer with a background in
                         Computer Science, specializing in Python and PyTorch for developing robust
-                        machine learning models. Proficient in S QL for efficient data manipulation
+                        machine learning models. Proficient in SQL for efficient data manipulation
                         and analysis, I am committed to leveraging my technical skills to drive
                         innovative solutions. My educational foundation and hands-on experience
                         equip me to tackle complex challenges and contribute effectively to
